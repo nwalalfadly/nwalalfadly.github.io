b4407a5a9c482bc2dba91870b780dd63096e4dfc
nwalalfadly <44496979+nwalalfadly@users.noreply.github.com>
2019-05-31 13:05:27 +0000

    Add files via upload

---
+++ jq.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+        $("#propos img").mouseenter(function(){
+        $(this).attr('src','img/26.png');
+       })
+       $("#propos img").mouseleave(function(){
+        $(this).attr('src','img/48.png');
+       })
+
+
+
+       $("header h1").mouseenter(function(){
+        $(this).css('color','#76FF03');
+      })
+    //   $("#section3").mouseleave(function(){
+    //   $(this).css("backgroundColor","rgb(30,30,47)");
+    //   })
+    //   $("#section3").mouseenter(function(){
+    //     $(this).css("backgroundColor","#D93A96");
+    //   })
+
+
+    //    $("#section5").mouseenter(function(){
+    //     $(this).css("backgroundColor","#EE8031");
+    //   })
+    //   $("#section5").mouseleave(function(){
+    //   $(this).css("backgroundColor","#EE8031");
+    //   })
+    //   $("#section5").mouseenter(function(){
+    //     $(this).css("backgroundColor","#D93A96");
+    //   })
+      
+               
+
+    });
+
